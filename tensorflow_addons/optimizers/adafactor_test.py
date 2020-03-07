f7bfd2ea296c234158a8d17e20e6c2840b830d7f
Smokrow <moritz.kroeger@tu-dortmund.de>
2020-03-07 08:14:01 +0100

    not working test with gradient tape. Rebaseing

--- tensorflow_addons/optimizers/adafactor_test.py
+++ tensorflow_addons/optimizers/adafactor_test.py
@@ -18,6 +18,7 @@ from __future__ import print_function
 
 import tensorflow as tf
 from tensorflow_addons.utils import test_utils
+import copy
 import numpy as np
 from adafactor import AdafactorOptimizer    
 
@@ -25,43 +26,69 @@ class AdafactorTest(tf.test.TestCase):
     def test_setup(self):
         a = AdafactorOptimizer()
         return
+
     @test_utils.run_in_graph_and_eager_modes(reset_test=True)
     def test_basic_usage(self):
-        mnist = tf.keras.datasets.mnist
-        (x_train, y_train),(x_test, y_test) = mnist.load_data()
-        x_train, x_test = x_train / 255.0, x_test / 255.0
+        x_train = np.random.rand(50,28,28)
+        y_train = np.random.randint(0,9,size=(50,10))
+
         model = tf.keras.models.Sequential([
           tf.keras.layers.Flatten(input_shape=(28, 28)),
           tf.keras.layers.Dense(128, activation='relu'),
           tf.keras.layers.Dense(10, activation='softmax')
         ])
-        optimizer = tf.keras.optimizers.Adam()
+
         optimizer = AdafactorOptimizer()
-        #model.compile(optimizer=optimizer,loss="mean_squared_error")
         model.compile(optimizer=optimizer,
-              loss='sparse_categorical_crossentropy',
+              loss='mean_squared_error',
               metrics=['accuracy'])
-        model.fit(x_train, y_train, epochs=10)
+        model.fit(x_train, y_train, epochs=1)
         return
-    
-    def gradient_tape(self):
-        x = np.random.rand(1,2,2)
-        y = np.random.rand(1,1)
-        model = tf.keras.Sequential([
-        tf.keras.layers.Flatten(input_shape=(2, 2)),
-        tf.keras.layers.Dense(1, activation='softmax')
-        ])
+
+    @test_utils.run_in_graph_and_eager_modes(reset_test=True)
+    def test_get_config(self):
         optimizer = AdafactorOptimizer()
-        with tf.GradientTape() as tape:
-            predictions = model(x)
-            loss = tf.keras.losses.mean_squared_error(y, predictions)
-        print(loss)
-        gradients = tape.gradient(loss, model.trainable_variables)
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        print(gradients)
+        config = optimizer.get_config()
+        self.assertIn("learning_rate",config)
+        self.assertIn("multiply_by_parameter_scale",config)
+        self.assertIn("decay_rate",config)
+        self.assertIn("beta1",config)
+        self.assertIn("clipping_threshold",config)
+        self.assertIn("factored",config)
+        self.assertIn("epsilon1",config)
+        self.assertIn("epsilon2",config)
 
 
+    @test_utils.run_in_graph_and_eager_modes(reset_test=True)
+    def test_change_variables(self):
+        val_0 = np.random.random((2,))
+        val_1 = np.random.random((2,))
 
+        var_0 = tf.Variable(val_0, dtype=tf.dtypes.float32)
+        var_1 = tf.Variable(val_1, dtype=tf.dtypes.float32)
+
+        grad_0 = tf.constant(
+            np.random.standard_normal((2,)), dtype=tf.dtypes.float32)
+        grad_1 = tf.constant(
+            np.random.standard_normal((2,)), dtype=tf.dtypes.float32)
+
+        grads_and_vars = list(zip([grad_0, grad_1], [var_0, var_1]))
+        optimizer = AdafactorOptimizer()
+
+        if tf.executing_eagerly():
+            var_0_before = copy.deepcopy(var_0)
+            var_1_before = copy.deepcopy(var_1)
+            optimizer.apply_gradients(grads_and_vars)
+            optimizer.apply_gradients(grads_and_vars)
+            self.assertNotEqual(tf.math.reduce_sum(var_0_before),tf.math.reduce_sum(var_0))
+            self.assertNotEqual(tf.math.reduce_sum(var_1_before),tf.math.reduce_sum(var_1))
+
+        else:
+            update = optimizer.apply_gradients(grads_and_vars)
+            self.evaluate(tf.compat.v1.global_variables_initializer())
+            self.assertNotEqual(np.sum(np.absolute(val_0)),self.evaluate(tf.math.reduce_sum(tf.math.abs(var_0))))
+            self.assertNotEqual(np.sum(np.absolute(val_1)),self.evaluate(tf.math.reduce_sum(tf.math.abs(var_1))))
+    
 
 if __name__ == "__main__":
     tf.test.main()
